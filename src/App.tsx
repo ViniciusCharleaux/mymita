58cd1756ae51e6787ceca14b2a2621a203ff968d
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2022-04-01 11:15:01 -0300

    feat: creating project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from './styles/global';
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * { 
+        margin: 0;
+        padding: 0;
+        border: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+    }
+    html{
+        scroll-behavior: smooth;
+        @media (max-width: 1080px){
+            font-size: 93.75%;
+        }
+        @media (max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+    :root {
+      --color-white: '#fff';    
+    }   
+    
+    
+    button{
+        cursor: pointer;
+    }
+`
