d13153d5f6f96de768699c911635eefbe423c5fe
Bruno Paulsen Fagundes <bruno.fagundes@alunos.ifsuldeminas.edu.br>
2022-04-27 22:29:16 -0300

    feat: signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,40 +3,55 @@ import { Container } from './styles';
 export const Signup: React.FC = () => {
     return (
         <Container>
-            <div className="head__welcome red">
-                <h1>Cadastro</h1>
-            </div>
-            <div className="head__welcome white">
-                <h1>cabeçalho</h1>
-                <div className="cadastro">
-                    <form method="post" action="">
-                        <h1>Login</h1>
-                        <p>
-                            <label for="nome">Nome</label>
-                            <input id="nome" name="nome" required="required" type="text" placeholder="Digite aqui..." />
-                        </p>
-                        <p>
-                            <label for="email">Email</label>
-                            <input id="email" name="email" required="required" type="email" placeholder="Digite aqui..." />
-                        </p>
-                        <p>
-                            <label for="senha">Senha</label>
-                            <input id="senha" name="senha" required="required" type="password" placeholder="Digite aqui..." />
-                        </p>
-                        <p>
-                            <label for="repitaSenha">Repita a senha</label>
-                            <input id="repitaSenha" name="repitaSenha" required="required" type="password" placeholder="Digite aqui..." />
-                        </p>
-
-                        <p>
-                            <input type="submit" value="Cadastrar" />
-                        </p>
-
-                        <a label="cancelar" href="./home/index.tsx">Cancelar</a>
-
-                    </form>
+            <main>
+                <div className="main__image">
+                    <h1>Cadastro</h1>
                 </div>
-            </div>
-        </Container>
+
+                <div className="main__signup-container">
+                    <div className="main__input-container">
+                        <label htmlFor="">nome:</label>
+                        <input
+                            type="text"
+                        //onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="main__input-container">
+                        <label htmlFor="">email:</label>
+                        <input
+                            type="email"
+                        //onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="main__input-container">
+                        <label htmlFor="">senha:</label>
+                        <input
+                            type="password"
+                        //onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="main__input-container">
+                        <label htmlFor="">repita a senha:</label>
+                        <input
+                            type="password"
+                        //onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+
+                    <button
+                        className='button__signup'
+                    //onClick={handleSignUp}
+                    >
+                        cadastrar
+                    </button>
+
+                    <a href="">cancelar</a>
+                </div>
+            </main>
+            <footer></footer>
+        </Container >
     );
 }
--- src/pages/signup/styles.ts
+++ src/pages/signup/styles.ts
@@ -1,30 +1,78 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 500vh;
+  height: 100vh;
   width: 100vw;
 
   display: flex;
-  flex-direction: line;
+  flex-direction: column;
+  align-items: center;
 
-  .head__welcome {
-    height: 25%;
-    width: 100%;
-    display: flex;
+  main {
+    width: 100%
+    height: 80%;
+    padding: 0 6rem 0;
+    
+    justify-content: space-between;
+    align-items: left;
 
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+    .main__signup-container {
+      height: 95%;
+      width: 32%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 1rem;
+      justify-content: space-around;
+      background-color: rgba(134, 123, 123, 0.14);
+      border-radius: 70px;
 
-    color: white;
+    .main__input-container{
+      display: flex;
+      flex-direction: column;
+      align-items: center ;
+    }
+
+    input{
+      height: 2.5rem;
+      width: 17rem;
+      border-radius: 20px;
+      box-shadow: 10px 5px 25px rgba(0, 0, 0, 0.25);
+      padding: 0 2rem ;
+    }
+
+    label{
+      font-family: 'fredoka one';
+    }
+
+    .button__signup {
+      width: 17rem;
+      height: 4.5rem;
+      background-color: var(--color-red);
+      border-radius: 40px;
+      color: var(--color-white);
+      font-size: 2.5rem;
+      transition: 0.3s box-shadow ease-in;
+      font-family: 'fredoka one';
+    }
+
+    .main__image {
+      height: 80%;
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      img{
+        width: 100%;
+      }
+
+    a{
+      color: var(--color-red);
+      text-decoration: underline;
+      font-family: 'fredoka one';
+    }
+
   }
 
-  .red {
-    background-color: var(--color-red);
-  }
-
-  .white {
-    background-color: var(--color-white);
-    color: var(--color-brown);
-  } 
 `
