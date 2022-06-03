09794430a8284b4c2fa3ea691da8afac343066da
Bruno Paulsen Fagundes <bruno.fagundes@alunos.ifsuldeminas.edu.br>
2022-06-03 10:33:35 -0300

    Criação do Modal Pedido
    Esqueleto do modal do pedido criado
    
    Co-Authored-By: Hiago Martins de Sousa <57905893+hiagosousa@users.noreply.github.com>

---
+++ src/pages/order/index.tsx
@@ -0,0 +1,25 @@
+import { Container } from "../dashboard/styles"
+
+export const Order: React.FC = () => {
+    return (
+        <Container>
+            <main>
+                <div>
+                    PEDIDO
+                </div>
+                <div className="guarnicoes">
+
+                </div>
+                <div className="mistura">
+
+                </div> 
+                <div className="tamanho">
+
+                </div>
+                <div className="botoes">
+
+                </div>
+            </main>
+        </Container>
+    )
+}
