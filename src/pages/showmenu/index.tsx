6b59367e2869f1b7b7bba9ca539dc6a8a8517a9e
Cintra <64995266+CintraB@users.noreply.github.com>
2022-06-09 22:33:32 -0300

    Preparando as páginas
    criando o ambiente para codar

---
+++ src/pages/showmenu/index.tsx
@@ -0,0 +1,113 @@
+import { Container } from './styles';
+import {Header} from '../../components/Header'
+import {FcGoogle} from 'react-icons/fc'
+import {BsFacebook} from 'react-icons/bs'
+import { images } from '../../constants';
+import { useState } from 'react';
+
+import {MiniLoading} from '../../components/Loading';
+
+// import {buscaLogin, createUser, CreateUserData} from '../../interfaces/user'
+
+import {useNavigate} from 'react-router-dom'
+
+import {useAuth} from '../../hooks/auth';
+import {useToast} from '../../hooks/toast';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+export const Home: React.FC = () => {
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [loading, setLoading] = useState<Boolean>(false);
+
+  const {signIn} = useAuth()
+  const {toastTopSuccess} = useToast()
+
+  const navigate = useNavigate()
+
+  const handleLogin = async() => {
+
+    setLoading(true)
+
+    const res = await signIn({email, password})
+
+    if(res){
+      toastTopSuccess('Login bem sucedido')
+      
+      setTimeout(() => {
+        setLoading(false);
+        navigate('/dashboard')
+      },1000);
+
+
+    }
+
+  }
+
+  return (
+    <Container>
+      <Header 
+        title1='cardápio'
+        title2='contato'
+      />
+      <main>
+        <div className='main__login-container'>
+
+          <div className="main__input-container">
+            <label htmlFor="">email:</label>
+            <input 
+              type="text" 
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="main__input-container">
+            <label htmlFor="">senha:</label>
+            <input 
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        
+
+          <button
+            className='button__login'
+            onClick={handleLogin}
+          >
+            login
+            {loading && <MiniLoading/>}
+          </button>
+
+          <div className="main__social">
+            <button
+              className='button__social'
+            >
+              <FcGoogle
+                size={40}
+               />
+            </button>
+            <button
+              className='button__social'
+            >
+              <BsFacebook
+                size={40}
+              />
+            </button>
+          </div>
+
+          <a href="/cadastro">não possui uma conta ?</a>
+
+        </div>
+        <div className='main__image'>
+          <img src={images.mainImg} alt="main image" />
+        </div>
+      </main>
+      <footer></footer>
+    </Container>
+  )
+}
---
+++ src/pages/showmenu/styles.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(180deg, rgba(255, 168, 0, 0.5) -23.56%, rgba(255, 255, 255, 0.5) 28.03%);
+
+  main {
+    width: 100%;
+    height: 80%;
+    display: flex;
+    flex-direction: row;
+    padding: 0 6rem 0;
+
+    justify-content: space-between;
+    align-items: center;
+
+    .main__login-container {
+      height: 95%;
+      width: 32%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 1rem;
+      justify-content: space-around;
+      background-color: rgba(134, 123, 123, 0.14);
+      border-radius: 70px;
+
+      a{
+        color: var(--color-red);
+        text-decoration: underline;
+        font-family: 'fredoka one';
+      }
+
+      .main__input-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center ;
+      }
+
+      input{
+        height: 3rem;
+        width: 17rem;
+        border-radius: 20px;
+        box-shadow: 10px 5px 25px rgba(0, 0, 0, 0.25);
+        padding: 0 2rem ;
+      }
+
+      label{
+        font-family: 'fredoka one';
+      }
+
+      .button__login {
+        width: 17rem;
+        height: 4.5rem;
+        background-color: var(--color-red);
+        border-radius: 40px;
+        color: var(--color-white);
+        font-size: 2.5rem;
+        transition: 0.3s box-shadow ease-in;
+        font-family: 'fredoka one';
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+
+      .main__social{
+        width: 60%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+
+        .button__social{
+          background-color: #fff;
+          height: 70px;
+          width: 70px;
+          border-radius: 20px;
+          border: 1px solid rgba(255, 168, 0, 1);
+        }
+      }
+    }
+
+    .main__image {
+      height: 80%;
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+
+      align-items: center;
+      justify-content: center;
+
+      img{
+        width: 100%;
+      }
+    }
+  }
+`
