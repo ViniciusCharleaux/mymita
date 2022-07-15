6ba0cefbd2fe282477bd1074d484a3e37729ff30
Cindel Sousa <cindelevelyn@gmail.com>
2022-07-15 09:46:06 -0300

    comanda props

--- src/components/Comanda/styles.ts
+++ src/components/Comanda/styles.ts
@@ -21,13 +21,13 @@ export const Container = styled.div`
 
   .btn-comanda {
     width: 100%;
-    height: 30%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
     margin-top: 45px;
-
+    
     button {
       width: 170px;
       height: 28px;
